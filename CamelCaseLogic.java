4d1838d3ed64c212b7e66e2eaa51751845664e57
Yogiraj Mohan khaladkar <105957964+yogikh2005@users.noreply.github.com>
2025-07-21 19:27:29 +0530

    21/7/25

--- CamelCaseLogic.java
+++ CamelCaseLogic.java
@@ -3,7 +3,7 @@ here is a sequence of words in CamelCase as a string of letters, , having the fo
 
 It is a concatenation of one or more words consisting of English letters.
 All letters in the first word are lowercase.
-For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
+For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase. 
 Given , determine the number of words in .
 
 Example
